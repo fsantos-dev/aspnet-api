05504a1b6a25daab986d36f6c4576b2b6e573f7a
Andres Santos <fsantos.development@gmail.com>
2026-07-16 10:46:43 -0500

    feat(Application): Agregar DTOs para Category

---
+++ src/MiApp.Application/Dtos/CategoryDto.cs
@@ -0,0 +1,14 @@
+namespace MiApp.Application.Dtos;
+
+public class CategoryDto
+{
+    public int Id {get; set;}
+    public string Name {get; set;} = string.Empty;
+
+    public string? Description {get; set;}
+
+    public bool IsActive {get; set;}
+
+    public DateTime CreatedAt {get; set;}
+
+}
