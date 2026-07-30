e7b3d5bc1aea1574dbb057c0c0b97b5a0a32f030
Andres Santos <fsantos.development@gmail.com>
2026-07-30 16:08:51 -0500

    feat(api): Registrar DBContext y repositorio en Program.cs

--- src/MiApp.API/Program.cs
+++ src/MiApp.API/Program.cs
@@ -2,35 +2,16 @@ using MiApp.Application.Interfaces;
 using MiApp.Application.Services;
 using MiApp.Domain.Interfaces;
 using MiApp.Infrastructure.Repositories;
+using MiApp.Infrastructure.Data;
 using Scalar.AspNetCore;
 using Microsoft.OpenApi;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-// Registrar DbContext con la cadena de conexión
-// builder.Services.AddDbContext<AppDbContext>(options =>
-// {
-//     options.UseSqlServer(
-//         builder.Configuration.GetConnectionString("DefaultConnection"));
-// });
-
-
 // 1. Agregar controladores
 builder.Services.AddControllers();
-
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
-});
 
 // 2. Configurar Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
@@ -47,9 +28,34 @@ builder.Services.AddOpenApi(options =>
 });
 builder.Services.AddSwaggerGen();
 
+
 // 3. Registrar Dependencias (Inyección de dependencias)
+
+// 3.1 Registrar el DbContext con la cadena de conexión
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// 3.2 Registrar el repositorio (Infrastructure)
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+// 3.3 Registrar el servicio (Application)
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+
+// 4. Agregar CORS
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 
 var app = builder.Build();
